ceed608fc29c217b471811023c94a970b55bb09e
Mostafasaadali <43938749+Mostafasaadali@users.noreply.github.com>
2026-09-18 07:47:54 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -722,7 +722,7 @@ if (prodSnap.exists()) {
     }
   };
 const fetchWithCache = async (collectionName, limitCount = 100) => {
-const cacheKey = `msa_${collectionName}_cache_v3`; 
+const cacheKey = `msa_${collectionName}_cache_v4`; 
 const timeKey = `msa_${collectionName}_time_v4`;
     const now = Date.now();
     
